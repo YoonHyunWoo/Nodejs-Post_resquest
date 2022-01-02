b5a0a190add37f9be0cfe6ee1c0b03e432392d2c
YoonHyenWoo <88831669+YoonHunWoo@users.noreply.github.com>
2022-01-02 23:20:11 +0900

    third commit

--- server.js
+++ server.js
@@ -3,20 +3,18 @@ var fs = require('fs');
 var querystring = require('querystring');
 const port = 3000;
 
-var server = http.createServer(function(req, res) {
+var server = app.use(function(req, res) {
 
     if(req.method == 'GET'){
         fs.readFile('./index.html' ,'utf8' ,function(error, data) {
-            res.writeHead(200, {'Content-Type' : 'text/html'});
-            res.end(data);
+            res.send(data);
         });
     }
     else if(req.method == 'POST'){
         req.on('data', function(chunk){
             console.log(chunk.toString());
             var data = querystring.parse(chunk.toString());
-            res.writeHead(200, {'Content-Type' : 'text/html'});
-            res.end('ID : ' + data.id + 'PW : ' + data.pw);
+            res.send('ID : ' + data.id + 'PW : ' + data.pw);
         });
     }
 
